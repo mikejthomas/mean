11847f88e9b74c0035915de9da251871b7e2834d
Michael Thomas <thomas.mikejohn@gmail.com>
2016-04-04 23:41:39 -0400

    pt1 12:31

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.listen('3000', function () {
+    console.log("Listening for Local Host 3000");
+    
+});
